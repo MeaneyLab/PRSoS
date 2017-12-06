fd428742065bbdd87f0d75a5d95bccb290cdc333
lchen-mcgill <lawrence.m.chen@mail.mcgill.ca>
2017-12-06 16:56:38 -0500

    Code correction

--- examples/prs_mdd_testsample.sh
+++ examples/prs_mdd_testsample.sh
@@ -21,7 +21,7 @@ test_sample_gen_GWAS_MDD_beta_clump_withaf \
 --sample_file test_sample.samples \
 --sample_file_skip 2 \
 --sample_file_delim " " \
---filetype VCF \
+--filetype GEN \
 --thresholds 1 0.5 0.4 0.3 0.2 0.1 0.05 0.01 0.001 0.0001 \
 --GWAS_delim "\t" \
 --snp_log
