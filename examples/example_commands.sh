818ab120496bff43c722484b48a496aabd1dbd16
Lawrence Chen <lawrence.m.chen@mail.mcgill.ca>
2017-12-21 18:27:19 -0500

    Update example files
    Previous version was missing the GWAS file in the examples. The current set allows you to use the examples_command.sh script to test.

---
+++ examples/example_commands.sh
@@ -0,0 +1,22 @@
+## CALCULATE PRS
+
+### using vcf file to create nine polygenic scores at sequential p-value thresholds 0.1, 0.15, 0.2, ..., 0.5
+
+spark-submit ../PRS_run.py \
+example.vcf \
+gwasfile.txt \
+example_vcf \
+--sample_file example.sample \
+--threshold_seq 0.1 0.5 0.05 \
+--snp_log
+
+### using gen file to create PRS while removing ambiguous SNPs
+
+spark-submit ../PRS_run.py \
+example.gen \
+gwasfile.txt \
+example_gen \
+--sample_file example.sample \
+--filetype GEN \
+--no_maf \
+--snp_log
